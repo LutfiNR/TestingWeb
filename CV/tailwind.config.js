a15076d10dd3afc5e07ea2452785f2e04e1a1edc
LutfiNR <lutfinurrohman5@gmail.com>
2023-01-16 20:11:53 +0700

    Tes CV

---
+++ CV/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors:{
+        'warna-biru': '#1a4380',
+      },
+      padding:{
+        '18': '4.5rem',
+      },
+    },
+  },
+  plugins: [],
+}
--- Lomba/README.md
+++
@@ -1,2 +0,0 @@
-# LombaBytesFest
-Repo lomba bytesfest 
